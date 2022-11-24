aa152d9a63a05a0619803c713e695549e057dc3a
Yuri Rezende <yuri.rezende@souunit.com.br>
2022-11-23 22:56:30 -0300

    Criação Game Mario

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const score = document.querySelector('.score');
+
+let count = 0;
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    
+    if (pipePosition <= 110 && pipePosition > 0 && marioPosition < 80){
+        
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './imagens/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval(loop);
+
+    }
+    count++;
+    score.innerHTML = `SCORE: ${count}`;
+}, 10)
+
+
+
+document.addEventListener('keydown', jump);
